5d48a9aa03e41fff024628deac3580f37f157ad5
Koval <artemKS333@yandex.kz>
2024-09-17 23:18:52 +0300

    добавил пременнные пути для новых эдпоинтов

--- src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
@@ -1,26 +1,33 @@
 package ru.yandex.practicum.catsgram.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.catsgram.model.Post;
 import ru.yandex.practicum.catsgram.service.PostService;
 
 import java.util.Collection;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/posts")
 public class PostController {
     private final PostService postService;
 
+    @Autowired
     public PostController(PostService postService) {
         this.postService = postService;
     }
 
-    @GetMapping
+    @GetMapping("/posts")
     public Collection<Post> findAll() {
         return postService.findAll();
     }
 
-    @PostMapping
+    @GetMapping("/posts/{id}")
+    public Optional<Post> findById(@PathVariable("id") long id) {
+        return postService.findById(id);
+    }
+
+    @PostMapping(value = "/post")
     public Post create(@RequestBody Post post) {
        return postService.create(post);
     }
--- src/main/java/ru/yandex/practicum/catsgram/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/UserController.java
@@ -1,16 +1,19 @@
 package ru.yandex.practicum.catsgram.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.catsgram.model.User;
 import ru.yandex.practicum.catsgram.service.UserService;
 
 import java.util.Collection;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
     private final UserService userService;
     
+    @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
     }
@@ -18,6 +21,11 @@ public class UserController {
     @GetMapping
     public Collection<User> findAll() {
         return userService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<User> findUserByMail(@PathVariable("id") long id) {
+        return userService.findById(id);
     }
 
     @PostMapping
